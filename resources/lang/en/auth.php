b7b38732fc95e2db28e14e72bfe0e9d08c6d33a2
mpartarrieu <mpartarrieu@gmail.com>
2016-04-19 12:16:34 +0200

    Remove pagination language file

--- resources/lang/en/auth.php
+++
@@ -1,7 +0,0 @@
-<?php
-//created on 2016-04-14 11:50:38
-return array (
-  'email.register.subject' => 'Welcome to Molista',
-  'failed' => 'These credentials do not match our records.',
-  'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-);
