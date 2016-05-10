d4c356774595246affd94d6529514bd40c5dc5d3
mpartarrieu <mpartarrieu@gmail.com>
2016-05-10 09:10:02 +0200

    Services codes

--- storage/lang/es/admin/properties/services.php
+++ storage/lang/es/admin/properties/services.php
@@ -1,18 +1,20 @@
 <?php
 
-return [
-    'empty' => 'No services found',
-    'created' => 'The service was successfully created.',
-    'saved' => 'The service was successfully updated.',
+	return [
+		'empty' => 'No services found',
+		'created' => 'The service was successfully created.',
+		'saved' => 'The service was successfully updated.',
 
-    'name' => 'Name',
-    'description' => 'Description',
-    'icon' => 'Icono',
-    'icon.help' => 'Size: 25 x 25 pixels',
-    'icon.error' => 'Please select a valid image.',
+		'name' => 'Name',
+		'description' => 'Description',
+		'code' => 'Código',
+		'code.error' => 'El código ya está en uso',
+		'icon' => 'Icono',
+		'icon.help' => 'Size: 25 x 25 pixels',
+		'icon.error' => 'Please select a valid image.',
 
-    'enabled' => 'Enabled',
+		'enabled' => 'Enabled',
 
-    'edit.title' => 'Edit service',
-    'create.title' => 'Create service',
-];
+		'edit.title' => 'Edit service',
+		'create.title' => 'Create service',
+	];
