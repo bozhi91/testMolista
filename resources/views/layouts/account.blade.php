d97bc3cbe65c3b59e8f8ef6617f68604ad3526ff
mpartarrieu <mpartarrieu@gmail.com>
2016-06-21 14:22:50 +0200

    Account: menu order and separators

--- resources/views/layouts/account.blade.php
+++ resources/views/layouts/account.blade.php
@@ -45,6 +45,7 @@
 		<div class="row">
 			<div class="col-sm-3 col-md-2 hidden-xs">
 				<ul class="nav nav-pills nav-stacked account-menu">
+
 					@permission('property-*')
 						<li role="presentation" class="{{ (@$submenu_section == 'properties') ? 'active' : '' }}">
 							<a href="{{ action('Account\PropertiesController@index') }}">
@@ -67,13 +68,15 @@
 							{{ Lang::get('account/menu.customers') }}
 						</a>
 					</li>
+					<li role="presentation" class="{{ (@$submenu_section == 'tickets') ? 'active' : '' }}">
+						<a href="{{ action('Account\TicketsController@getIndex') }}">
+							<i class="account-icon account-icon-ticket"></i>
+							{{ Lang::get('account/menu.tickets') }}
+						</a>
+					</li>
+					<li class="separator"></li>
+
 					@role('company')
-						<li role="presentation" class="{{ (@$submenu_section == 'marketplaces') ? 'active' : '' }}">
-							<a href="{{ action('Account\MarketplacesController@getIndex') }}">
-								<i class="account-icon account-icon-marketplaces"></i>
-								{{ Lang::get('account/menu.marketplaces') }}
-							</a>
-						</li>
 						<li role="presentation" class="{{ (@$submenu_section == 'reports') ? 'active' : '' }}">
 							<a href="javascript:;" data-toggle="collapse" data-target="#account-submenu-reports" aria-expanded="false" class="{{ (@$submenu_section == 'reports') ? '' : 'collapsed' }}">
 								<i class="account-icon account-icon-reports"></i>
@@ -85,27 +88,35 @@
 								<li><a href="{{ action('Account\Reports\LeadsController@getIndex') }}" class="{{ (@$submenu_subsection == 'reports-leads') ? 'current' : '' }}">{{ Lang::get('account/menu.reports.leads') }}</a></li>
 							</ul>
 						</li>
+						<li class="separator"></li>
 					@endrole
-					<li role="presentation" class="{{ (@$submenu_section == 'tickets') ? 'active' : '' }}">
-						<a href="{{ action('Account\TicketsController@getIndex') }}">
-							<i class="account-icon account-icon-ticket"></i>
-							{{ Lang::get('account/menu.tickets') }}
-						</a>
-					</li>
-					@permission('site-*')
-						<li role="presentation" class="{{ (@$submenu_section == 'site') ? 'active' : '' }}">
-							<a href="javascript:;" id="account-menu-btn-site" data-toggle="collapse" data-target="#account-submenu-site" aria-expanded="false" class="{{ (@$submenu_section == 'site') ? '' : 'collapsed' }}">
-								<i class="account-icon account-icon-settings"></i>
-								{{ Lang::get('account/menu.site') }}
-							</a>
-							<ul id="account-submenu-site" class="nav {{ (@$submenu_section == 'site') ? '' : 'collapse' }}" role="menu" aria-labelledby="account-menu-btn-site">
-								<li><a href="{{ action('Account\Site\ConfigurationController@getIndex') }}" class="{{ (@$submenu_subsection == 'site-configuration') ? 'current' : '' }}">{{ Lang::get('account/menu.site.configuration') }}</a></li>
-								<li><a href="{{ action('Account\Site\WidgetsController@getIndex') }}" class="{{ (@$submenu_subsection == 'site-widgets') ? 'current' : '' }}">{{ Lang::get('account/menu.site.widgets') }}</a></li>
-								<li><a href="{{ action('Account\Site\MenusController@index') }}" class="{{ (@$submenu_subsection == 'site-menus') ? 'current' : '' }}">{{ Lang::get('account/menu.site.menus') }}</a></li>
-								<li><a href="{{ action('Account\Site\PagesController@index') }}" class="{{ (@$submenu_subsection == 'site-pages') ? 'current' : '' }}">{{ Lang::get('account/menu.site.pages') }}</a></li>
-							</ul>
-						</li>
-					@endpermission
+
+					@if ( Auth::user()->hasRole('company') || Auth::user()->can('site-*') )
+						@permission('site-*')
+							<li role="presentation" class="{{ (@$submenu_section == 'site') ? 'active' : '' }}">
+								<a href="javascript:;" id="account-menu-btn-site" data-toggle="collapse" data-target="#account-submenu-site" aria-expanded="false" class="{{ (@$submenu_section == 'site') ? '' : 'collapsed' }}">
+									<i class="account-icon account-icon-settings"></i>
+									{{ Lang::get('account/menu.site') }}
+								</a>
+								<ul id="account-submenu-site" class="nav {{ (@$submenu_section == 'site') ? '' : 'collapse' }}" role="menu" aria-labelledby="account-menu-btn-site">
+									<li><a href="{{ action('Account\Site\ConfigurationController@getIndex') }}" class="{{ (@$submenu_subsection == 'site-configuration') ? 'current' : '' }}">{{ Lang::get('account/menu.site.configuration') }}</a></li>
+									<li><a href="{{ action('Account\Site\WidgetsController@getIndex') }}" class="{{ (@$submenu_subsection == 'site-widgets') ? 'current' : '' }}">{{ Lang::get('account/menu.site.widgets') }}</a></li>
+									<li><a href="{{ action('Account\Site\MenusController@index') }}" class="{{ (@$submenu_subsection == 'site-menus') ? 'current' : '' }}">{{ Lang::get('account/menu.site.menus') }}</a></li>
+									<li><a href="{{ action('Account\Site\PagesController@index') }}" class="{{ (@$submenu_subsection == 'site-pages') ? 'current' : '' }}">{{ Lang::get('account/menu.site.pages') }}</a></li>
+								</ul>
+							</li>
+						@endpermission
+						@role('company')
+							<li role="presentation" class="{{ (@$submenu_section == 'marketplaces') ? 'active' : '' }}">
+								<a href="{{ action('Account\MarketplacesController@getIndex') }}">
+									<i class="account-icon account-icon-marketplaces"></i>
+									{{ Lang::get('account/menu.marketplaces') }}
+								</a>
+							</li>
+						@endrole
+						<li class="separator"></li>
+					@endif
+
 					<li role="presentation" class="{{ (@$submenu_section == 'home') ? 'active' : '' }}">
 						<a href="{{ action('AccountController@index') }}">
 							<i class="account-icon account-icon-info"></i>
