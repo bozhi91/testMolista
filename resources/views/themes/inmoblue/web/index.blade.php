82804b1891296710c3f16849d42fd3a28039d38e
David Pérez <dp@incubout.com>
2016-06-28 17:08:55 +0200

    Finishing header footer and index page

--- resources/views/themes/inmoblue/web/index.blade.php
+++ resources/views/themes/inmoblue/web/index.blade.php
@@ -176,10 +176,10 @@
 
 			var main_property = cont.find('.main-property');
 			var main_property_image = main_property.find('.main-image');
-			if ( main_property_image.length > 0 && main_property.height() > main_property_image.height() ) {
-				main_property_image.addClass('hide');
-				main_property.find('.item.active').css({ 'background-image': 'url(' + main_property_image.attr('src') + ')' })
-			}
+		
+			main_property_image.addClass('hide');
+			main_property.find('.item.active').css({ 'background-image': 'url(' + main_property_image.attr('src') + ')' })
+		
 
 		});
 	</script>
