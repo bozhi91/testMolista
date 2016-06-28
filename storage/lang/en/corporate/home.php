80014f85162b113c9712f12056fc30d9b9d1d200
mpartarrieu <mpartarrieu@gmail.com>
2016-06-28 17:05:38 +0200

    Textos home

--- storage/lang/en/corporate/home.php
+++ storage/lang/en/corporate/home.php
@@ -6,12 +6,15 @@
 		'h2' => 'Multiply your Real Estate business possibilities',
 		'features' => '<p><strong>molista</strong> gives you everything you need to make <strong>your Real Estate business present on the Internet</strong>:</p>
 		<ul>
+		<li data-type="web">Web site for your company</li>
 		<li data-type="fixa">Detailed webpages for all your properties</li>
 		<li data-type="searcher">Property search with advanced  filters</li>
 		<li data-type="relation">Ability to link properties</li>
 		<li data-type="easy">Easy to use and manage</li>
 		<li data-type="settings">Customizable</li>
 		<li data-type="responsive">Compatible with phones and tablets</li>
+		<li data-type="agents">Agent management</li>
+		<li data-type="docs">Document management</li>
 		</ul>',
 
 		'h3' => 'The features you need',
