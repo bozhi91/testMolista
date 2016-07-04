5cdf095cdd8e9fcfbb35da1ab3bdc921e3c26adf
Albert <farwel@gmail.com>
2016-07-04 17:55:39 +0200

    BienesOnline: mismo formato que trovit

---
+++ app/Marketplaces/BienesOnline/BienesOnline.php
@@ -0,0 +1,5 @@
+<?php namespace App\Marketplaces\BienesOnline;
+
+class BienesOnline extends \App\Marketplaces\Trovit\Trovit {
+
+}
---
+++ app/Marketplaces/BienesOnline/Writer.php
@@ -0,0 +1,5 @@
+<?php namespace App\Marketplaces\BienesOnline;
+
+class Writer extends \App\Marketplaces\Trovit\Writer {
+
+}
