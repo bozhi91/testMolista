cfc802495972363d84d905774aab60f4db5f6421
Albert <farwel@gmail.com>
2016-07-07 10:21:38 +0200

    Atributos de Clase

--- app/Marketplaces/Clasf/Clasf.php
+++ app/Marketplaces/Clasf/Clasf.php
@@ -4,4 +4,21 @@ class Clasf extends \App\Marketplaces\XML {
 
     protected $iso_lang = 'es';
 
+    protected $configuration = [
+        [
+            'block' => 'contact_data',
+            'fields' => [
+                [
+                    'name' => 'email',
+                    'type' => 'text',
+                    'required' => true
+                ],
+                [
+                    'name' => 'contact_data',
+                    'type' => 'textarea'
+                ]
+            ]
+        ]
+    ];
+
 }
