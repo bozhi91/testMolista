5323a06bf1d29b8b4f476ff7d3d7713bf88fc49c
mpartarrieu <mpartarrieu@gmail.com>
2016-07-11 17:40:41 +0200

    Corporate: signup email layout

--- resources/views/emails/corporate/signup.blade.php
+++ resources/views/emails/corporate/signup.blade.php
@@ -2,46 +2,79 @@
 
 @section('content')
 
-	{!! Lang::get('corporate/signup.email.hello', [
-		'name' => @$owner_name,
-	]) !!}
+	<div class="container">
 
-	@if ( empty($pending_request) )
-		{!! Lang::get('corporate/signup.email.features.intro.now') !!}
-		{!! Lang::get('corporate/signup.email.features.list') !!}
-	@elseif ( $pending_request['payment_method'] == 'stripe' )
-		{!! Lang::get('corporate/signup.email.features.intro.stripe', [
-			'plan' => @$pending_request->plan_name,
-			'price' => @$pending_request->plan_price,
-			'period' => @$pending_request->payment_interval,
-			'priceperiod' => Lang::get("web/plans.price.{$pending_request->payment_interval}") . ' ' . price($pending_request->plan_price, [ 'decimals'=>0 ]),
-			'paymethod' => Lang::get('account/payment.method.stripe'),
-		]) !!}
-		{!! Lang::get('corporate/signup.email.features.list') !!}
-	@elseif ( $pending_request['payment_method'] == 'transfer' )
-		{!! Lang::get('corporate/signup.email.features.intro.transfer', [
-			'plan' => @$pending_request->plan_name,
-			'price' => @$pending_request->plan_price,
-			'period' => @$pending_request->payment_interval,
-			'priceperiod' => Lang::get("web/plans.price.{$pending_request->payment_interval}") . ' ' . price($pending_request->plan_price, [ 'decimals'=>0 ]),
-			'paymethod' => Lang::get('account/payment.method.transfer'),
-			'iban' => @$pending_request->iban_account,
-		]) !!}
-		{!! Lang::get('corporate/signup.email.features.list') !!}
-	@endif
+		<div class="content">
 
-	{!! Lang::get('corporate/signup.email.url.site', [
-		'site_url' => @$site_url,
-	]) !!}
+			<div class="header">
+				<a href="{{ LaravelLocalization::getLocalizedURL(null, '/') }}" target="_blank"><img src="{{ asset('images/emails/logo.png') }}" alt="" /></a>
+			</div>
 
-	{!! Lang::get('corporate/signup.email.url.account', [
-		'account_url' => @$account_url,
-		'email' => @$owner_email,
-	]) !!}
+			<div class="h1">{{ Lang::get('corporate/signup.email.subject') }}</div>
 
-	@if ( @$pending_request['payment_method'] == 'stripe' )
-		<p>&nbsp;</p>
-		{!! Lang::get('corporate/signup.email.warning.stripe') !!}
-	@endif
+			<div class="text-gray">
+				{!! Lang::get('corporate/signup.email.hello', [
+					'name' => @$owner_name,
+				]) !!}
+			</div>
+
+			@if ( empty($pending_request) )
+				<div class="text-gray">
+					{!! Lang::get('corporate/signup.email.features.intro.now') !!}
+				</div>
+				{!! Lang::get('corporate/signup.email.features.list') !!}
+			@elseif ( $pending_request['payment_method'] == 'stripe' )
+				<div class="text-gray">
+					{!! Lang::get('corporate/signup.email.features.intro.stripe', [
+						'plan' => @$pending_request->plan_name,
+						'price' => @$pending_request->plan_price,
+						'period' => @$pending_request->payment_interval,
+						'priceperiod' => Lang::get("web/plans.price.{$pending_request->payment_interval}") . ' ' . price($pending_request->plan_price, [ 'decimals'=>0 ]),
+						'paymethod' => Lang::get('account/payment.method.stripe'),
+					]) !!}
+				</div>
+				{!! Lang::get('corporate/signup.email.features.list') !!}
+			@elseif ( $pending_request['payment_method'] == 'transfer' )
+				<div class="text-gray">
+					{!! Lang::get('corporate/signup.email.features.intro.transfer', [
+						'plan' => @$pending_request->plan_name,
+						'price' => @$pending_request->plan_price,
+						'period' => @$pending_request->payment_interval,
+						'priceperiod' => Lang::get("web/plans.price.{$pending_request->payment_interval}") . ' ' . price($pending_request->plan_price, [ 'decimals'=>0 ]),
+						'paymethod' => Lang::get('account/payment.method.transfer'),
+						'iban' => @$pending_request->iban_account,
+					]) !!}
+				</div>
+				{!! Lang::get('corporate/signup.email.features.list') !!}
+			@endif
+
+			<div class="text-center website-block">
+				{!! Lang::get('corporate/signup.email.url.site', [
+					'site_url' => @$site_url,
+				]) !!}
+				<p>
+					<a href="{{ @$site_url }}" target="_blank" class="website-link">{{ @$site_url}}</a>
+				</p>
+			</div>
+
+			{!! Lang::get('corporate/signup.email.url.account', [
+				'account_url' => @$account_url,
+				'email' => @$owner_email,
+			]) !!}
+
+			@if ( @$pending_request['payment_method'] == 'stripe' )
+				<div class="payment-completed">
+					{!! Lang::get('corporate/signup.email.warning.stripe') !!}
+				</div>
+			@endif
+
+		</div>
+
+		<div class="footer">
+			<a href="{{ env('LINKS_FACEBOOK', 'https://www.facebook.com/') }}" target="_blank"><img src="{{ asset('images/emails/footer-facebook.png') }}" alt="" /></a>
+			<a href="{{ env('LINKS_LINKEDIN', 'https://www.linkedin.com/') }}" target="_blank"><img src="{{ asset('images/emails/footer-linkedin.png') }}" alt="" /></a>
+		</div>
+
+	</div>
 
 @endsection
