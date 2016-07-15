d52c5582cfeb85e77247c9534de55a00e2a13392
Albert <farwel@gmail.com>
2016-07-15 12:22:22 +0200

    Refactor marketplaces Base and XML

--- app/Marketplaces/Base.php
+++ app/Marketplaces/Base.php
@@ -6,6 +6,19 @@ abstract class Base implements MarketplaceInterface {
 
     protected $configuration = [];
 
+    protected $iso_lang;
+    protected $config;
+
+    public function __construct(array $config = [])
+    {
+        if (empty($this->iso_lang))
+        {
+            throw new \LogicException(static::class." must declare the attribute $iso_lang.");
+        }
+
+        $this->config = $config;
+    }
+
     public function getMarketplaceConfiguration()
     {
         return $this->configuration;
