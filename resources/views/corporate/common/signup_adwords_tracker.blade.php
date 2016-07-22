2366bf2d79d4a598f3a8041e87ee8460f7205631
mpartarrieu <mpartarrieu@gmail.com>
2016-07-22 11:42:21 +0200

    Google Adwords Tracker

---
+++ resources/views/corporate/common/signup_adwords_tracker.blade.php
@@ -0,0 +1,19 @@
+@if ( App::environment() == 'production' || App::environment() == 'development' )
+	<!-- Google Code for Setup Conversion Page -->
+	<script type="text/javascript">
+		/* <![CDATA[ */
+		var google_conversion_id = 961870289;
+		var google_conversion_language = "en";
+		var google_conversion_format = "3";
+		var google_conversion_color = "ffffff";
+		var google_conversion_label = "gdgbCLH4w1gQ0fPTygM";
+		var google_remarketing_only = false;
+		/* ]]> */
+	</script>
+	<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js"></script>
+	<noscript>
+		<div style="display:inline;">
+			<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/961870289/?label=gdgbCLH4w1gQ0fPTygM&amp;guid=ON&amp;script=0"/>
+		</div>
+	</noscript>
+@endif
