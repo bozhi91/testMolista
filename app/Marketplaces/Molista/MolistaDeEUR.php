47ed9194aab53f18c63da07fe0740e4858f15351
Albert <farwel@gmail.com>
2016-08-02 16:04:15 +0200

    Feed molesta por idiomas y monedas (EN, ES, DE, EUR, USD)

---
+++ app/Marketplaces/Molista/MolistaDeEUR.php
@@ -0,0 +1,8 @@
+<?php namespace App\Marketplaces\Molista;
+
+class Molista extends Molista {
+
+    protected $iso_lang = 'de';
+    protected $currency = 'EUR';
+
+}
---
+++ app/Marketplaces/Molista/MolistaEnGBP.php
@@ -0,0 +1,8 @@
+<?php namespace App\Marketplaces\Molista;
+
+class Molista extends Molista {
+
+    protected $iso_lang = 'en';
+    protected $currency = 'GBP';
+
+}
