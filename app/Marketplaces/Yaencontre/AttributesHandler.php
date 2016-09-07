bd29de2d1e520dbde447b06e5084d032f10f3a1f
Albert <farwel@gmail.com>
2016-09-07 17:20:46 +0200

    Listado de ciudades ordenado por nombre

--- app/Marketplaces/Yaencontre/AttributesHandler.php
+++ app/Marketplaces/Yaencontre/AttributesHandler.php
@@ -82,6 +82,17 @@ class AttributesHandler {
 				];
 			}
 
+			// Sort by name
+			usort($attribute['values'], function($a, $b) {
+				$unwanted_array = array(    'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+                            'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
+                            'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c',
+                            'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
+                            'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y' );
+				
+			    return strtr($a['label'], $unwanted_array) > strtr($b['label'], $unwanted_array);
+			});
+
 			// Save values file
 			file_put_contents($valuesFile, '<?php return '. var_export($attribute['values'], true).';');
 		}
