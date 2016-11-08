84207605a71921c05517810c202da7bbd5022711
David Pérez <dp@incubout.com>
2016-11-08 18:07:24 +0100

    Commit with the discount price applied

--- resources/views/web/properties/row-price.blade.php
+++ resources/views/web/properties/row-price.blade.php
@@ -1,3 +1,3 @@
 <div class="price {{@$price_class}}">
-	<a href="{{ $item->full_url }}">{{ price($item->price, $item->infocurrency->toArray()) }}</a>
+	<a href="{{ $item->full_url }}">{{ price($item->price, $item->infocurrency->toArray()) }} @include('web.properties.discount-price') </a>
 </div>
