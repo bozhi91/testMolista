682d2f587b26cb966fc7beac1a28c59c20ace298
Albert <farwel@gmail.com>
2016-11-09 12:24:36 +0100

    Fix if site is missing

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -47,10 +47,13 @@ class Controller extends BaseController
 
 		$this->__initialize();
 
-		if ($this->site->id == env('VILLAMED_ID')) {
+		if ($this->site && $this->site->id == env('VILLAMED_ID')) {
 			$this->site->header_class = 'header-logo-md';
 		}
-		view()->share('header_class', $this->site->header_class);
+
+		if ($this->site) {
+			view()->share('header_class', $this->site->header_class);
+		}
 	}
 
 	public function __initialize() {}
