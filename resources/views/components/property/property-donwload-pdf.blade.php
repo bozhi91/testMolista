0c9787205b1e45f6ac3c9ac2df396ada7fa19228
David Pérez <dp@incubout.com>
2017-03-13 11:13:50 +0100

    Fix property download button label

--- resources/views/components/property/property-donwload-pdf.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="property-download-pdf">
-	<a href="{{ action('Web\PropertiesController@downloads', [ $property->slug, LaravelLocalization::getCurrentLocale() ]) }}" class="btn btn-primary hidden-xs" target="_blank">{{ Lang::get('web/properties.download.pdf') }}</a>
-</div>
