9601b42b22a28a225e057d68cce7203b0aed8351
mpartarrieu <mpartarrieu@gmail.com>
2017-05-25 16:03:31 +0200

    Código hubspot

---
+++ resources/views/corporate/common/hubspot.blade.php
@@ -0,0 +1,5 @@
+@if ( ! env('APP_DEBUG', false) )
+	<!-- Start of HubSpot Embed Code -->
+	<script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/2271461.js"></script>
+	<!-- End of HubSpot Embed Code -->
+@endif
