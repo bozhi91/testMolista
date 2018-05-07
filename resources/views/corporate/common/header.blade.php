04199ef9b0112e50a7ab7e7ae845c252cc2d9649
bozhi <bozhidar1991@gmail.com>
2018-05-07 18:05:49 +0200

    Limit hte access to the report in the free plan.

--- resources/views/corporate/common/header.blade.php
+++ resources/views/corporate/common/header.blade.php
@@ -9,7 +9,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="{{ action('CorporateController@index') }}">Molista</a>
+			<a class="navbar-brand" style="color:#ff0000" href="{{ action('CorporateController@index') }}">Molista</a>
 		</div>
 
 		<div class="collapse navbar-collapse" id="app-navbar-collapse">
