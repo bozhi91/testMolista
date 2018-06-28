ec22c545cc730c3ea7d9c715e7b378a3ef48311b
bozhi <bozhidar1991@gmail.com>
2018-06-28 15:34:43 +0200

    Refactor: A minor update in the Blog. Bug Fixed.

--- database/migrations/2018_06_12_151902_create_posts_table.php
+++ database/migrations/2018_06_12_151902_create_posts_table.php
@@ -18,8 +18,6 @@ class CreatePostsTable extends Migration {
             $table->string('title');
             $table->string('body');
             $table->dateTime('created_at')->nullable();
-
-            $table->foreign('site_id')->references('id')->on('sites');
         });
 
 
